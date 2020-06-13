36ca5b12819b9f6377fcaccd1487e84a6f6ef333
John Pham <mr.john.pham@gmail.com>
2020-06-13 05:44:35 -0400

    Create hello world response

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/plain '});
-    res.write('Hello there');
-    res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.write(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello there!</h1>
+        </body>
+    </html>
+    `);
+
+    res.end(); //does this send or "execute" the response?
+    //console.dir(req);
+
+    //console.dir(res);
+
 
 }).listen(8080);
 
