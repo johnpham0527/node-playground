6d1e4325971ae6536b4f4dc6caaf065ac324d4c3
John Pham <mr.john.pham@gmail.com>
2020-06-14 05:47:11 -0400

    Implement switch statement to route requests

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createReadStream } = require('fs');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/html'});
+    //res.writeHead(200, { 'Content-Type': 'text/html'});
     
     // res.write(`
     // <!DOCTYPE html>
@@ -22,23 +22,33 @@ http.createServer((req, res) => {
     // res.end();
     
 
-    res.end(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Hello there!</h1>
-                <h2>Practicing Node's http module</h2>
-            </body>
-        </html>
-        `, 
-        "utf8",
-        cb => {
-            console.log("Finished sending back response")
-    });
+    // res.end(`
+    //     <!DOCTYPE html>
+    //     <html>
+    //         <head>
+    //             <meta charset="UTF-8">
+    //             <title>Hello World!</title>
+    //         </head>
+    //         <body>
+    //             <h1>Hello there!</h1>
+    //             <h2>Practicing Node's http module</h2>
+    //         </body>
+    //     </html>
+    //     `, 
+    //     "utf8",
+    //     cb => {
+    //         console.log("Finished sending back response")
+    // });
+
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            createReadStream("./index.html").pipe(res);
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('404 - Page Not Found', cb => console.log(`Page not found: ${req.url}`));
+    }
 
     console.log(req.url);
 
