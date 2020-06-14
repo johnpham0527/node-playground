bb0b610fbc3fa6cc9380d0f29217c7cd640ddc98
John Pham <mr.john.pham@gmail.com>
2020-06-14 05:51:22 -0400

    Implement serveFile function

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const { createReadStream } = require('fs');
+const { create } = require('domain');
 
-
+const serveFile = (status, contentType, fileName, res) => {
+    res.writeHead(status, {'Content-Type': contentType});
+    createReadStream(fileName).pipe(res);
+}
 
 http.createServer((req, res) => {
 
     console.log(req.url);
     switch (req.url) {
         case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            createReadStream("./index.html").pipe(res);
-            break;
+            return serveFile('200', 'text/html', './index.html', res);
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('404 - Page Not Found', cb => console.log(`Page not found: ${req.url}`));
